fb70f5461d0a0956ea4f12d7b3f924c72bcc3270
Hip Lunatic <djensen81@gmail.com>
2023-10-05 10:42:12 -0400

    added firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import  { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
       <Route path ='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
+        <Route path="/signin" element={<SignIn />} />
       </Route>
     </Routes>
   )
